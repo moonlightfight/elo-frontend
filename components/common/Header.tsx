5b47ac49b48a80505a58c6fc4f529363c4a528e5
Corey Lanier <iam@coreylanier.com>
2021-05-11 00:19:50 -0400

    added header shadow

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="bg-hero w-full h-96 bg-top bg-fixed relative">
+    <div className="bg-hero w-full h-96 bg-top bg-fixed relative shadow-md">
       <div className="font-heading absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-blue-200 text-center">
         <h1 className="text-5xl">Sailor Moon S Power Rankings</h1>
         <h2 className="text-2xl">Who's the best at fighting evil by moonlight?</h2>
