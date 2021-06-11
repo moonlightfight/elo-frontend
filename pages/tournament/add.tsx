414bbbe2d26166c5774f6b0778d3c48446cd90d6
Corey Lanier <iam@coreylanier.com>
2021-06-11 00:17:25 -0400

    creating tournament route pages

---
+++ pages/tournament/add.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Add() {
+  return (
+    <div>
+      <Head>
+        <title>Add Tournament - SMS Global Rankings</title>
+      </Head>
+      <h2 className="text-center text-xl font-bold">Add Tournament</h2>
+    </div>
+  )
+}
