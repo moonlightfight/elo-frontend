f3c6229000e564fd8dad0b888724523302af7854
Corey Lanier <iam@coreylanier.com>
2021-08-22 14:36:57 -0400

    extend tailwind and fonts

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -6,8 +6,8 @@
 <Router {routes} />
 
 <style global>
+  @import url("https://fonts.googleapis.com/css?family=Dosis|Great+Vibes&display=swap");
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
 </style>
-
--- svelte/tailwind.config.js
+++ svelte/tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        hero: "linear-gradient(to right, rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('/images/heroimage.jpeg')",
+      }),
+    },
+    fontFamily: {
+      heading: ["Great Vibes", "cursive"],
+      body: ["Dosis", "sans-serif"],
+    },
+    boxShadow: {
+      full: "12px 12px 5px 1px rgba(0,0,0,0.75)",
+      half: "6px 6px 5px 1px rgba(0,0,0,0.75)",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
