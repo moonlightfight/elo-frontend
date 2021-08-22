9b7a1c1c739725d2d8e0598da623fcc71f2c87ba
Corey Lanier <iam@coreylanier.com>
2021-08-22 17:26:03 -0400

    create header buttons

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -12,4 +12,43 @@
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+  .button {
+    @apply bg-pink-900;
+    @apply text-yellow-100;
+    @apply uppercase;
+    @apply px-4;
+    @apply py-2;
+    @apply rounded-sm;
+    @apply font-bold;
+    @apply shadow-full;
+    @apply cursor-pointer;
+    transition: all 0.3s ease-in-out;
+  }
+
+  .button:hover {
+    @apply shadow-half;
+  }
+
+  .button:disabled {
+    box-shadow: none;
+  }
+
+  .text-field {
+    @apply w-full;
+    @apply mb-4;
+    @apply border-blue-200;
+    @apply border-b-4;
+    @apply rounded-md;
+    @apply px-2;
+    @apply py-1;
+    transition: all 0.3s ease-in-out;
+  }
+
+  .text-field:focus {
+    @apply border-pink-200;
+  }
+
+  .text-field:disabled {
+    @apply border-purple-200;
+  }
 </style>
--- svelte/src/components/Header.svelte
+++ svelte/src/components/Header.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import { _ } from "svelte-i18n";
+  import { url, goto } from "@roxi/routify";
   $: userToken = localStorage.getItem("elo_token");
+
+  const logout = (): void => {
+    localStorage.removeItem("elo_token");
+    $goto("/");
+  };
 </script>
 
 <div class="bg-hero w-full h-96 bg-top bg-fixed relative shadow-md">
@@ -9,5 +15,26 @@
   >
     <h1 class="text-5xl">{$_("header.title")}</h1>
     <h2 class="text-2xl">{$_("header.subtitle")}</h2>
+  </div>
+  <div
+    class={`absolute right-4 top-4 font-body flex ${
+      userToken === null ? "w-48" : "w-1/3"
+    } justify-around`}
+  >
+    <a href={$url("/")} class="button">{$_("header.home")}</a>
+    {#if userToken}
+      <a href={$url("/add-admin")} class="button">{$_("header.add.admin")}</a>
+      <a href={$url("/tournament/add")} class="button"
+        >{$_("header.add.tournament")}</a
+      >
+      <button
+        on:click={() => {
+          logout();
+        }}
+        class="button">{$_("header.logout")}</button
+      >
+    {:else}
+      <a href={$url("/login")} class="button">{$_("header.login")}</a>
+    {/if}
   </div>
 </div>
