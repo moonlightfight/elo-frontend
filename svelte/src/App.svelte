a72401fdcf5fe647bd4ef298cdc9d8ab81869179
Corey Lanier <iam@coreylanier.com>
2021-08-22 01:50:51 -0400

    feat: begin switch from next to svelte

---
+++ svelte/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<Router {routes} />
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
