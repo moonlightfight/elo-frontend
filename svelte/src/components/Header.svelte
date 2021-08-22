74b6f1395ae6219ef816dc9e552883c692d26319
Corey Lanier <iam@coreylanier.com>
2021-08-22 14:37:49 -0400

    create header component

---
+++ svelte/src/components/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n";
+  const userToken = localStorage.getItem("elo_token");
+</script>
+
+<div class="bg-hero w-full h-96 bg-top bg-fixed relative shadow-md">
+  <div
+    class="font-heading absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-blue-200 text-center"
+  >
+    <h1 class="text-5xl">{$_("header.title")}</h1>
+    <h2 class="text-2xl">{$_("header.subtitle")}</h2>
+  </div>
+</div>
