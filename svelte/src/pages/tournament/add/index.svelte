6cb1ed3c6e953791050ff8824da68e40b51b38fc
Corey Lanier <iam@coreylanier.com>
2021-08-22 23:02:23 -0400

    prepare data for adding tournaments

---
+++ svelte/src/pages/tournament/add/index.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n";
+  import Select from "svelte-select";
+  import { goto } from "@roxi/routify";
+
+  // type imports
+  import type {
+    TournamentInfo,
+    PlayerInfo,
+    MatchInfo,
+    Character,
+    ApiPlayer,
+    PlayerSetup,
+  } from "./_types";
+
+  import { getCountry } from "../../../helpers/countries";
+
+  $: countries = getCountry();
+  let tournamentUrl: string = "";
+
+  const getTournament = () => {};
+</script>
