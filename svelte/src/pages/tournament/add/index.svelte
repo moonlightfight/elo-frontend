6e10560b77c9be47c3d4ec63757bac9a1a3c5642
Corey Lanier <iam@coreylanier.com>
2021-08-22 23:46:49 -0400

    setup tournament state

--- svelte/src/pages/tournament/add/index.svelte
+++ svelte/src/pages/tournament/add/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { _ } from "svelte-i18n";
+  import axios from "axios";
   import Select from "svelte-select";
   import { goto } from "@roxi/routify";
 
@@ -12,11 +13,43 @@
     ApiPlayer,
     PlayerSetup,
   } from "./_types";
+  import type { CountryInfo } from "../../../helpers/_types";
 
   import { getCountry } from "../../../helpers/countries";
 
-  $: countries = getCountry();
+  $: countries = [];
+
+  $: apiPlayers = [];
+
+  $: characters = [];
+
+  let players: PlayerSetup[] = [];
+
+  $: {
+    async () => {
+      getCountry().then((res: CountryInfo[]) => {
+        countries = res;
+      });
+      axios.get("http://localhost:8080/api/players").then((res) => {
+        const data: ApiPlayer[] = res?.data;
+        apiPlayers = data;
+      });
+      axios.get("http://localhost:8080/api/characters").then((res) => {
+        const data: Character[] = res?.data;
+        characters = data;
+      });
+    };
+  }
+
   let tournamentUrl: string = "";
 
-  const getTournament = () => {};
+  let tournament: TournamentInfo | null;
+
+  const getTournament = async () => {};
+
+  const addPlayer = async () => {};
+
+  const setPlayer = async () => {};
+
+  const submitTournament = async () => {};
 </script>
